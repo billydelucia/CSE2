1311292a50fc253bdc88afa492e5cd0348b2dfb0
billydelucia <wld217@lehigh.edu>
2014-09-19 23:08:18 +0000

    comleted course number hmework

--- hw04/CourseNumber.java
+++ hw04/CourseNumber.java
@@ -2,3 +2,45 @@
 //Billy DeLucia
 //Course Number Homework
 //
+import java.util.Scanner;
+public class CourseNumber{
+    public static void main(String[ ] args){
+        Scanner input= new Scanner(System.in);
+        int courseNumber;
+        System.out.print("Enter a six digit number giving the course semester- ");
+        //
+        if (input.hasNextInt()){
+            courseNumber=input.nextInt();
+            boolean test1= (courseNumber%10==0 && courseNumber%100<=40);
+            boolean test2= (courseNumber%1000==0);
+            boolean legit= (test1 && !test2);
+            if (courseNumber<186510 || courseNumber>201440){
+                System.out.println("The number was outsid the range [186510,201440]");
+                return;
+            }
+            else if (!legit){
+                System.out.println(courseNumber+ " is not a legitimate semester");
+            }
+            else{
+                int year= (int)(courseNumber/100);
+                int semester= courseNumber%100;
+                if (semester==10){
+                    System.out.println("The course was offerd in the Spring semester of " +year);
+                }
+                else if (semester==20){
+                    System.out.println("The course was offerd in the Summer 1 semester of " +year);
+                }
+                else if (semester==30){
+                    System.out.println("The course was offerd in the Summer 2 semester of " +year);
+                }
+                else if (semester==40){
+                    System.out.println("The course was offerd in the Fall semester of " +year);
+                }
+            }    
+        }    
+        else{
+            System.out.println("You did not enter an int");
+            return;
+        }
+    }
+}

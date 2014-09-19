4e54931a8ef42a951a0a8dad3dfdc828f4a02bf1
billydelucia <wld217@lehigh.edu>
2014-09-19 22:40:36 +0000

    completed month homework and updated income tax

--- PLay/Play.java
+++ PLay/Play.java
@@ -4,13 +4,9 @@ import java.util.Scanner;
 
  public class Play {
      public static void main(String [ ] args) {
-         System.out.println("This program multiplies a number by ten");
-         double ten=10;
-         Scanner input= new Scanner(System.in);
-         System.out.println("Enter number:");
-         double number= input.nextDouble( );
-         
-         double result=ten*number;
-         System.out.println("Ten times your number is " +result+ " ");
+         int first=1;
+         int second=2;
+         boolean test=first>second;
+         System.out.println(test);
      }
  }
--- hw04/Month.java
+++ hw04/Month.java
@@ -2,3 +2,59 @@
 //Billy DeLucia
 // Month Homework
 //
+import java.util.Scanner;
+public class Month{
+    public static void main(String[ ] args){
+        Scanner input=new Scanner(System.in);
+        int month;
+        boolean odd;
+        System.out.print("Enter and int giving the number of the month (1-12)- ");
+        //
+        if (input.hasNextInt()) {
+            month=input.nextInt();
+            odd= (month%2)==1;
+            if (month>1 || month<12){
+                if (odd){
+                    System.out.println("The month has 31 days");
+                }
+                else{
+                    if (month==2){   //Febuary is a special case due to leap years
+                        System.out.print("Enter and int giving the year ");
+                        int year;
+                        if (input.hasNextInt()){
+                            year=input.nextInt();
+                            boolean divisibleBy4= (year%4==0);
+                            boolean divisibleBy100= (year%100==0);
+                            boolean divisibleby400= (year%400==0);
+                            if (divisibleBy4 && !divisibleBy100){
+                                System.out.println("The month has 29 days");
+                            }
+                            else if (divisibleBy4 && divisibleBy100 && divisibleby400){
+                                System.out.println("The month has 29 days");
+                            }
+                            else{
+                                System.out.println("The month has 28 days");
+                            }
+                            
+                        }
+                        else{
+                            System.out.println("You did not enter an int");
+                            return; //terminates program
+                        }
+                    }
+                    else{
+                        System.out.println("The month has 30 days");
+                    }
+                }
+                
+            }
+            else{
+                System.out.println("You did not enter an int between 1 and 12");
+            }
+        }
+        else{
+            System.out.println("You did not enter an int");
+            return; //terminates program
+        }
+    }
+}

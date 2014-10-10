4972c372a07a342cd5fed3a9304e6c6021aad83c
billydelucia <wld217@lehigh.edu>
2014-10-10 19:51:39 +0000

    created BaseTransformer a program for tanslating bases

---
+++ Play/BaseTransformer.java
@@ -0,0 +1,182 @@
+//////////////////////////////////////////////////////////////////////
+//Billy DeLucia
+//
+// The goal of this program is to be able to translate differnt bases into base 10
+// The accepoted bases should inclued binary octal and heidcimal.
+// GOOD LUCK!!!
+
+import java.util.Scanner;
+public class BaseTransformer {
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		System.out.println("This program translates diffent bases into decimal.");
+		System.out.println("Accpeted bases are binary, base 2, octal, base 8, and hexidecimal, base 16");
+		System.out.println("Please input the base of your input");
+		
+		// Check if desired input base is acceptable 
+		if (!input.hasNextInt()){
+			System.out.println("You did not enter an integer");
+			return;
+		}
+		int base=input.nextInt();
+		boolean goodInput= base==2 || base==8 || base==16;
+		if (!goodInput){
+			System.out.println("You entered an uspupported base, not 2, 8, or 16");
+			return;
+		}
+		
+		// asking for input in base 
+		System.out.println("Enter number in correct form of input base");
+		/*
+		if (!input.hasNextInt()){
+			System.out.println("You did not enter an integer");
+			return;
+		}
+		*/
+		int count=0, basepower=1, digit=0, add=0, sum=0, max=100;
+		
+		int number=0;
+		switch (base){
+			case 2:
+			case 8:
+				if (!input.hasNextInt()){
+					System.out.println("You did not enter an integer");
+					return;
+				}	
+				number=input.nextInt();
+			break;
+			case 16:
+				String hexidecimal;
+				String done="done";
+				char hex;
+				System.out.println("Enter each didgit idividually hitting 'enter' between each");
+				System.out.println("Ender 'done' after last digit");
+				while (true){
+					hexidecimal=input.next();
+					if (hexidecimal.equals(done)){
+						break;
+					}
+					hex=hexidecimal.charAt(0);
+					int remainder=number;
+					switch (hex){
+						case '1':
+							add=1;
+							break;
+						case '2':
+							add=2;
+							break;
+						case '3':
+							add=3;
+							break;
+						case '4':
+							add=4;
+							break;
+						case '5':
+							add=5;
+							break;
+						case '6':
+							add=6;
+							break;
+						case '7':
+							add=7;
+							break;
+						case '8':
+							add=8;
+							break;
+						case '9':
+							add=9;
+							break;
+						case 'a':
+						case 'A':
+							add=10;
+							break;
+						case 'b':
+						case 'B':
+							add=11;
+							break;
+						case 'c':
+						case 'C':
+							add=12;
+							break;
+						case 'd':
+						case 'D':
+							add=13;
+							break;
+						case 'e':
+						case 'E':
+							add=14;
+							break;
+						case 'f':
+						case 'F':
+							add=15;
+							break;
+					}
+					//digit=remainder%10;
+					//remainder/=10;
+					basepower*=base;
+					if (count==0){
+						basepower=1;
+					}
+					add*=basepower;
+					sum+=add;
+					count++;
+					
+				}
+				
+		}
+		int remainder=number, check=0;
+		
+		// check for correct format of base
+		switch (base){
+			case 2:		//Binary
+				while (remainder>0){
+					check=remainder%10;
+					remainder/=10;
+					if (check>base-1){
+						System.out.println("Incorrect binary format");
+						System.out.println(check);
+						return;
+					}
+				}
+				break;
+			case 8:		//Octal
+				while (remainder>0){
+					check=remainder%10;
+					remainder/=10;
+					if (check>base-1){
+						System.out.println("Incorrect octal format");
+						System.out.println(check);
+						return;
+					}
+				}
+				break;
+			case 16:	//Hexidecimal
+				System.out.println("This base is currently in beta mode");
+				break;
+		}
+		
+		//calculate decimal value
+		// int count=0, basepower=1, digit=0, add=0, sum=0, max=100;
+		remainder=number;
+		switch (base){
+			case 2:
+			case 8:
+				while (remainder>0){
+					digit=remainder%10;
+					remainder/=10;
+					basepower*=base;
+					if (count==0){
+						basepower=1;
+					}
+					add=digit*basepower;
+					sum+=add;
+					count++;
+					if (count>max){
+						System.out.println("max cycles reached");
+						break;
+					}
+				}
+		}
+		System.out.println("In decimal your input is "+sum);
+	}
+}

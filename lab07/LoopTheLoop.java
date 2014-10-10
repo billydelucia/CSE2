1e0677a416c2b97b62527f18f4512fa4b1b67ba4
billydelucia <wld217@lehigh.edu>
2014-10-10 18:02:38 +0000

    updated hw06

--- lab07/LoopTheLoop.java
+++ lab07/LoopTheLoop.java
@@ -5,34 +5,43 @@
 import java.util.Scanner;
 public class LoopTheLoop {
     public static void main(String[] args){
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter an int between 1 and 15");
-        if (!input.hasNextInt()){
-            System.out.println("You did not enter an int");
-            return;
-        }
-        int nStars=input.nextInt();
-        if (nStars<1 || nStars>15){
-            System.out.println("Your int was not within the range [1,15]");
-            return;
-        }
-        int x=0;
-        while (x<nStars){
-            System.out.print("*");
-            x++;
-        }
-        System.out.println("");
-        x=1;
-        int y=0;
-        while (x<=nStars){
-            while (y<=0){
-                System.out.print("*");
-                y++;
+        boolean infinite= true;
+        while (infinite){
+            Scanner input=new Scanner(System.in);
+            System.out.print("Enter an int between 1 and 15 - ");
+            if (!input.hasNextInt()){
+                System.out.println("You did not enter an int");
+                return;
             }
-            x++;
-            y-=x;
+            int nStars=input.nextInt();
+            if (nStars<1 || nStars>15){
+                System.out.println("Your int was not within the range [1,15]");
+                return;
+            }
+            
+            
+            int x=0;
+            while (x<nStars){
+                System.out.print("*");
+                x++;
+            }
             System.out.println("");
             
+            
+            x=1;
+            int y=0;
+            while (x<=nStars){
+                while (y<=0){
+                    System.out.print("*");
+                    y++;
+                }
+                x++;
+                y-=x;
+                System.out.println("");
+            }
+            System.out.print("Enter y or Y to go again- ");
+            char exit=input.next().charAt(0);
+            infinite= exit=='y' || exit== 'Y';
         }
     }
 }

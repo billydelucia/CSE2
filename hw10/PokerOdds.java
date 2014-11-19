08e6332499f5b9721ac2d40986bc4109089c5a40
billydelucia <wld217@lehigh.edu>
2014-11-19 04:13:32 +0000

    z

--- hw10/PokerOdds.java
+++ hw10/PokerOdds.java
@@ -126,7 +126,8 @@ public class PokerOdds{
     
     public static void simulateOdds(){
         
-        int add=0;
+        int A=0, K=0, Q=0, J=0, two=0, three=0, four=0, five=0, six=0, seven=0, eight=0, nine=0, ten=0;
+        int total=0;
         for (int v=0; v<10000; v++){
             int [] deck= new int [52];
             for (int i=0; i<52; i++){
@@ -160,13 +161,67 @@ public class PokerOdds{
                     }
                     int pair2=hand[g];
                     if (pair1%13==pair2%13){
-                       add++; 
+                        total++;
+                       switch (pair1%13){
+                            case 0 :
+                                A++;
+                                break;
+                            case 1 :
+                                K++;
+                                break;
+                            case 11 :
+                                J++;
+                                break;
+                            case 12 :
+                                Q++;
+                                break;
+                            case 2:
+                                two++;
+                                break;
+                            case 3:
+                                three++;
+                                break;
+                            case 4:
+                                four++;
+                                break;
+                            case 5:
+                                five++;
+                                break;
+                            case 6:
+                                six++;
+                                break;
+                            case 7:
+                                seven++;
+                                break;
+                            case 8:
+                                eight++;
+                            case 9:
+                                nine++;
+                                break;
+                            case 10:
+                                ten++;
+                                break;
+                        } 
                     }
                 }
             }
         
         }
-        int pairs=add/2;
-        System.out.println(add +" "+(10000-pairs) );
+         A/=2; K/=2; Q/=2; J/=2; two/=2; three/=2; four/=2; five/=2; six/=2; seven/=2; eight/=2; nine/=2; ten/=2;
+         total/=2;
+        System.out.println("A   "+A);
+        System.out.println("K   "+K);
+        System.out.println("Q   "+Q);
+        System.out.println("J   "+J);
+        System.out.println("10  "+ten);
+        System.out.println("9   "+nine);
+        System.out.println("8   "+eight);
+        System.out.println("7   "+seven);
+        System.out.println("6   "+six);
+        System.out.println("5   "+five);
+        System.out.println("4   "+four);
+        System.out.println("3   "+three);
+        System.out.println("2   "+two);
+        System.out.println("Total not exactly one pair "+(10000-total));
     }
 }
